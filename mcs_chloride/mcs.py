6f14287dae3a26a89873b43f54d0b50258dfc066
SaintDragon2 <saintdragon2@gmail.com>
2015-07-07 09:36:41 +0900

    track examples

--- mcs_chloride/mcs.py
+++ mcs_chloride/mcs.py
@@ -3,15 +3,15 @@ __author__ = 'SungYong'
 from mcs_chloride.mcs_cl_fdm import ReliabilityClFdm
 
 
-runs = 10000
+runs = 1000
 
 file_name = 'src294.csv'
 
 for i in range(0, runs):
     r_fdm = ReliabilityClFdm()
     r_fdm.read_file(file_name)
-    r_fdm.calculate(20000)
+    r_fdm.calculate(900)
 
-    tracking = open(file_name[:-3] + 'track' + str(i), 'w')
+    tracking = open(file_name[:-3] + 'track_' + str(i), 'w')
     r_fdm.write_traking_elements(tracking, [5020, 5260])
     tracking.close()
